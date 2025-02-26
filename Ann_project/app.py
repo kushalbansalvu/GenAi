cea861e1d49ca3d68413659e01272354f7cf7bcd
Kushal Bansal <kushaler2010@gmail.com>
2025-02-26 22:13:29 +1100

    Added ann and rnn practical implementation

--- Ann_project/app.py
+++ Ann_project/app.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
+import os
 
 # Load the trained model
 model = tf.keras.models.load_model('model.h5')
